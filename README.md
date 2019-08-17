79e8cb42aaa48c7469af08d11dbbbc7454c794f4
lukehedger <luke.hedger@gmail.com>
2019-08-17 20:29:06 +0100

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# untab
-Unsplash for Firefox
+<p align='center'>
+  <img src='https://user-images.githubusercontent.com/1913316/63214928-d7794100-c116-11e9-93fe-b5470427b291.png' width='128'/>
+</p>
+
+<p align='center'>
+  <img src='https://user-images.githubusercontent.com/1913316/63216444-4feafc80-c12d-11e9-99a5-2140871428fe.png' width='1392'/>
+</p>
---
+++ untab.js
@@ -0,0 +1,49 @@
+(async () => {
+  try {
+    const res = await fetch(
+      "https://api.unsplash.com/photos/random?query=architecture nature&orientation=landscape",
+      {
+        headers: {
+          Authorization:
+            "Client-ID 5e209ac631fe3db30edae82fad81153b0018104909e6b2d8c3c201267df021c3"
+        }
+      }
+    );
+
+    const json = await res.json();
+
+    const elUntab = document.getElementById("untab");
+    elUntab.style = `background-image: url(${json.urls.regular})`;
+
+    const elProfileImage = document.getElementById("profile_image");
+    elProfileImage.style = `background-image: url(${
+      json.user.profile_image.medium
+    })`;
+
+    const userLink = document.createElement("a");
+    userLink.href = json.user.links.html;
+    userLink.textContent = json.user.name;
+
+    const onSpan = document.createElement("span");
+    onSpan.textContent = " on ";
+
+    const photoLink = document.createElement("a");
+    photoLink.href = json.links.html;
+    photoLink.textContent = "Unsplash";
+
+    const elAttribution = document.getElementById("attribution");
+    elAttribution.appendChild(userLink);
+    elAttribution.appendChild(onSpan);
+    elAttribution.appendChild(photoLink);
+
+    const location = document.createElement("p");
+    location.id = "location_text"
+    location.textContent = json.location.title;
+
+    const elLocation = document.getElementById("location");
+    elLocation.appendChild(location);
+  } catch (error) {
+    const elUntab = document.getElementById("untab");
+    elUntab.textContent = "Oops. We couldn't fetch an image from Unsplash.";
+  }
+})();
